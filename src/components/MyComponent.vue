d2fc30fdc0e1a046f17d60b9c6bedf7802732c4a
Diego Santos <santos.diegoferreira@gmail.com>
2017-07-19 02:51:27 -0300

    Initial commit.

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>My Component</h1>
+    <p>{{ content }}</p>
+    <button @click="clickMe('Parameter data')">Click me!</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-component',
+    data () {
+      return {
+        content: 'This is the content'
+      }
+    },
+    methods: {
+      clickMe: function (param) {
+        this.content = param
+      }
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import MyComponent from './components/MyComponent.vue'
+
+Vue.component('my-component', MyComponent)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
